dbc06740be2e09a6017afdb19afc1c5ad55495d9
Keith Poncardas <poncardask03@gmail.com>
2025-03-10 16:31:05 +0800

    Dashboard improved

--- controller/private.js
+++ controller/private.js
@@ -1,25 +1,60 @@
 const logger = require("../console/logger");
 const { BookGenres } = require('../utils/options');
-const { getBooks, uploadBook } = require("../service/bookService");
+const { getBooks, uploadBook, editBook, viewBook, deleteBook } = require("../service/bookService");
 const { getNavbarConfig } = require("../utils/navbarConfig");
 
 const retrieveHomeDashboard = async (req, res) => {
   try {
     const books = await getBooks({ query: req.query });
-
     res.render('private/home', { ...books, BookGenres, ...getNavbarConfig('dashboard') });
   } catch (err) {
     logger.error(`Error parsing data: ${err.message}`);
   }
 };
 
+const retrieveUploadForm = async (req, res) => {
+  res.render('private/upload', { ...getNavbarConfig('dashboard'), BookGenres });
+};
+
+const retrieveEditForm = async (req, res) => {
+  const { id } = req.params;
+  try {
+    const book = await viewBook({ bookId: id });
+    logger.success(book.title);
+    console.log(book);
+    res.render('private/edit', { book, ...getNavbarConfig('dashboard'), BookGenres });
+  } catch (err) {
+    logger.error(err.message);
+  }
+}
+
 const uploadNewBook = async (req, res) => {
   try {
     const newBook = await uploadBook({ book: req.body });
-    logger.success(`Book uploaded succesfully: ${newBook}`);
+    logger.success(`Book uploaded: ${newBook}`);
     res.redirect('/dashboard');
   } catch (err) {
-    logger.error(`Error uploading new book: ${err.message}`);
+    logger.error(`Error posting book: ${err.message}`);
+  }
+};
+
+const updateBook = async (req, res) => {
+  try {
+    const editedBook = await editBook({ bookId: req.params.id, bookData: req.body });
+    logger.success(`Book updated: ${editedBook}`);
+    res.redirect('/dashboard');
+  } catch (err) {
+    logger.error(`Error edit this book: ${err.message}`);
+  }
+}
+
+const removeBook = async (req, res) => {
+  try {
+    const deletedBook = await deleteBook({ bookId: req.params.id });
+    logger.success(deletedBook);
+    res.redirect('/dashboard');
+  } catch (err) {
+    logger.error(err);
   }
 }
 
@@ -28,4 +63,4 @@ const logout = (req, res) => {
   res.redirect('/auth/login');
 };
 
-module.exports = { retrieveHomeDashboard, uploadNewBook, logout };
+module.exports = { retrieveHomeDashboard, retrieveUploadForm, uploadNewBook, retrieveEditForm, updateBook, removeBook, logout };
--- routes/privateRoutes.js
+++ routes/privateRoutes.js
@@ -1,10 +1,14 @@
 const express = require('express');
-const { retrieveHomeDashboard, logout, uploadNewBook } = require('../controller/private');
+const { retrieveHomeDashboard, logout, retrieveUploadForm, uploadNewBook, retrieveEditForm, updateBook, removeBook } = require('../controller/private');
 const router = express.Router();
 
 router.get('/', retrieveHomeDashboard);
+router.get('/upload', retrieveUploadForm);
+router.get('/:id/edit', retrieveEditForm);
 router.get('/logout', logout);
 
 router.post('/upload', uploadNewBook);
+router.put('/:id/edit', updateBook);
+router.delete('/:id/delete', removeBook);
 
 module.exports = router;
